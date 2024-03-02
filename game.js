3f7d8c831094c3c21c2720bb667a95d6ff8cf701
MuktaJainn <jain.mukta20032002@gmail.com>
2024-03-02 17:06:04 +0530

    2nd Day

--- game.js
+++ game.js
@@ -14,6 +14,7 @@ let birdWidth = 34;
 let birdHeight =24;
 let birdX = boardW/8;
 let birdY = boardh/2;
+let birdImg;
 
 
 //We are creating our bird object.
@@ -27,6 +28,18 @@ let bird ={
 }
 
 
+//pipes
+
+let pipes =[]; //Multiple pipes throughout
+let pipeWidth = 64; //width/height = 384/3072 = 1/8
+let pipeHeight =512;
+let pipeX =boardW;
+let pipeY  = 0;
+
+let Toppipe;
+let BottomPipe;
+
+
 
 
 window.onload = function(){
@@ -36,9 +49,53 @@ window.onload = function(){
     context = board.getContext("2d"); //used for drawing on the board.
 
 
+    // //Let's draw the bird.
 
-    //Let's draw the bird.
+    // context.fillStyle = "green";
+    // context.fillRect(bird.x , bird.y , bird.width , bird.height);
 
+
+    //load images.
+
+    birdImg= new Image();
+    birdImg.src ="./flappybird.png";
     
+    birdImg.onload = function(){
+        context.drawImage(birdImg,bird.x, bird.y, bird.width,bird.height);
+    }
 
+    //pipe
+
+    Toppipe = new Image();
+    Toppipe.src = "./toppipe.png";
+
+    BottomPipe = new Image();
+    Toppipe.src = "./bottompipe.png";
+
+    requestAnimationFrame(update);
+    setInterval(placePipes, 1500);
+
+
+}
+
+function update(){
+    requestAnimationFrame(update);
+    context.clearRect(0,0, board.width, board.height);
+
+
+    //bird
+    context.drawImage(birdImg,bird.x, bird.y, bird.width,bird.height);
+
+}
+
+
+function placePipes(){
+    let toppipe ={
+        img: Toppipe,
+        x :  pipeX,
+        y:   pipeY,
+        width: pipeWidth ,
+        height: pipeHeight 
+
+    }
 }
